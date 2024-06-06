722e94db8575a30fa583040a7b8a256e2ff55cdd
Florian Feigl <florian.feigl@gmail.com>
2024-06-06 11:17:16 +0200

    added: keymaps added C-u/d -> C-uzz/dzz

--- nvim/.config/nvim/lua/plugins/vim-settings.lua
+++ nvim/.config/nvim/lua/plugins/vim-settings.lua
@@ -7,4 +7,8 @@ return {
   vim.cmd 'set shiftwidth=2',
 
   vim.cmd 'set nu rnu',
+
+  -- Custom Mappings
+  vim.api.nvim_set_keymap('n', '<C-d>', '<C-d>zz', { noremap = true, silent = true }),
+  vim.api.nvim_set_keymap('n', '<C-u>', '<C-u>zz', { noremap = true, silent = true })
 }
