9f7e3a151dd6d52d7922d2c0962e237dcff6f679
Florian Feigl <florian.feigl@gmail.com>
2024-07-19 13:05:12 +0200

    updated: which-key.lua

--- nvim/.config/nvim/lua/plugins/chat-gpt.lua
+++ nvim/.config/nvim/lua/plugins/chat-gpt.lua
@@ -1,15 +1,15 @@
 -- chat-gpt.lua
-
 return {
   "jackMort/ChatGPT.nvim",
-    event = "VeryLazy",
-    config = function()
-      require("chatgpt").setup()
-    end,
-    dependencies = {
-      "MunifTanjim/nui.nvim",
-      "nvim-lua/plenary.nvim",
-      "folke/trouble.nvim",
-      "nvim-telescope/telescope.nvim"
-    }
+  event = "VeryLazy",
+  config = function()
+    require("chatgpt").setup()
+      api_key_cmd = "pass show api/tokens/openai"
+  end,
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+    "nvim-lua/plenary.nvim",
+    "folke/trouble.nvim",
+    "nvim-telescope/telescope.nvim"
+  }
 }
--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -1,12 +1,11 @@
 -- plugins/lualine.lua
-
 return {
   'nvim-lualine/lualine.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   config = function()
     require('lualine').setup {
       options = {
-        theme = 'catppuccin',
+        theme = 'auto',
       },
     }
   end,
