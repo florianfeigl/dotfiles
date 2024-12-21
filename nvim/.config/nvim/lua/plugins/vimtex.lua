280328ff9cdb13ab0ed5ebd7fb3a138e69c13688
Florian Feigl <florian.feigl@gmail.com>
2024-12-21 22:16:39 +0100

    commit: test

--- nvim/.config/nvim/lua/plugins/vimtex.lua
+++ nvim/.config/nvim/lua/plugins/vimtex.lua
@@ -1,10 +1,16 @@
 -- plugins/vimtex.lua
 
 return {
-    'lervag/vimtex',
-    config = function()
-      vim.g.vimtex_view_method = 'zathura'  -- Setze deinen bevorzugten PDF-Viewer
-      vim.g.vimtex_compiler_method = 'latexmk'  -- Verwende latexmk zum Kompilieren
-    end,
-    ft = 'tex',  -- Lade das Plugin nur für TeX-Dateien
-  }
+  "lervag/vimtex",
+  config = function()
+    vim.g.vimtex_view_method = "zathura" -- Use Zathura for PDF preview
+    vim.g.vimtex_compiler_latexmk = {
+      build_dir = "build", -- Use a separate build directory for output files
+    }
+    vim.g.vimtex_view_general_options = "--synctex-forward @line:@col:@file"
+    vim.cmd([[
+        let g:vimtex_view_general_options_latexmk = '--synctex-editor-command "nvr --remote-silent +%l %f"'
+        ]])
+  end,
+  ft = 'tex',  -- Lade das Plugin nur für TeX-Dateien
+}
