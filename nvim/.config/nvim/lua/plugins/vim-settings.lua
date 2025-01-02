402e8aec0b52286697bb5e937fca33e69a42529a
Florian Feigl <florian.feigl@gmail.com>
2025-01-02 12:16:00 +0100

    fixed: kanata

--- nvim/.config/nvim/lua/plugins/vim-settings.lua
+++ nvim/.config/nvim/lua/plugins/vim-settings.lua
@@ -8,6 +8,7 @@ return {
 
   vim.cmd 'set nu rnu',
 
+  vim.cmd 'filetype plugin indent on',
   -- Custom Motion Mappings
   vim.api.nvim_set_keymap('n', '<C-d>', '<C-d>zz', { noremap = true, silent = true }),
   vim.api.nvim_set_keymap('n', '<C-u>', '<C-u>zz', { noremap = true, silent = true }),
