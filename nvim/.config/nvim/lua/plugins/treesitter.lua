0daf81fe2852675f7ade7ff2b3935e5b9bed9f8b
Florian Feigl <florian.feigl@gmail.com>
2025-05-07 02:59:38 +0200

    waybar re-configured, ricing up some stuff :)

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,16 +1,29 @@
 -- plugins/treesitter.lua
 
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	config = function()
-		local configs = require("nvim-treesitter.configs")
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    local configs = require("nvim-treesitter.configs")
 
-		configs.setup({
-			ensure_installed = { "c", "lua", "vim", "vimdoc", "javascript", "html", "python", "rust", "css", "sql" },
-			sync_install = false,
-			highlight = { enable = true },
-			indent = { enable = true },
-		})
-	end,
+    configs.setup({
+      ensure_installed = { "c", "lua", "vim", "vimdoc", "javascript", "html", "python", "rust", "css", "sql" },
+      sync_install = false,
+      highlight = { enable = true },
+      indent = { enable = true },
+
+      textobjects = {
+        select = {
+          enable = true,
+          lookahead = true,
+          keymaps = {
+            ["af"] = "@function.outer",
+            ["if"] = "@function.inner",
+            ["ac"] = "@class.outer",
+            ["ic"] = "@class.inner",
+          },
+        },
+      },
+    })
+  end,
 }
