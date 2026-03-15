7ed9aff2f527adc575a760027b2a4c2bc75c050e
Florian Feigl <florian.feigl@gmail.com>
2026-03-15 11:37:29 +0100

    fix: Catppuccin theme immediate loading and priority
    - Set priority to 1000 to ensure early loading
    - Add vim.cmd.colorscheme() to apply theme immediately on startup
    - Disable dim_inactive for consistent colors
    - Configure styles (italic comments, etc.)
    - Ensure LazyVim doesn't override colorscheme

--- dot_config/nvim/lua/plugins/catppuccin.lua
+++ dot_config/nvim/lua/plugins/catppuccin.lua
@@ -4,11 +4,31 @@ return {
 	{
 		"catppuccin/nvim",
 		lazy = false,
+		priority = 1000,
 		name = "catppuccin",
 		config = function()
 			require("catppuccin").setup({
 				flavour = "macchiato",
+				dim_inactive = {
+					enabled = false,
+				},
+				styles = {
+					comments = { "italic" },
+					conditionals = { "italic" },
+					loops = {},
+					functions = {},
+					keywords = {},
+					strings = {},
+					variables = {},
+					numbers = {},
+					booleans = {},
+					properties = {},
+					types = {},
+					operators = {},
+				},
 			})
+			-- Apply the colorscheme immediately
+			vim.cmd.colorscheme("catppuccin")
 		end,
 	},
 	{
